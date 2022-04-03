5354b0d64ca4820c043ec0f5483e6a0063edf8b2
amirulislamhridoy <hossainhridoy30687@gmail.com>
2022-04-03 12:17:29 +0600

    not found page add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import OrderReview from './components/OrderReview/OrderReview';
 import GrandPa from './components/GrandPa/GrandPa';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/orderreview" element={<OrderReview />}></Route>
         <Route path="/grandpa" element={<GrandPa></GrandPa>}></Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 style={{textAlign: 'center', color: 'red'}}>NotFound</h1>
+            <h1 style={{textAlign: 'center', color: 'red'}}>404</h1>
+        </div>
+    );
+};
+
+export default NotFound;
